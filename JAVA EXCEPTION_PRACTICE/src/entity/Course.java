3b5d709255beedb9b97764fe6e3cf10dd8ac80bb
Dung Pham <dungpham197qn@gmail.com>
2025-04-04 16:41:27 +0700

    Ă'

---
+++ JAVA EXCEPTION_PRACTICE/src/entity/Course.java
@@ -0,0 +1,15 @@
+package entity;
+
+import java.util.Scanner;
+
+public class Course implements IApp{
+
+    String courseID;
+    String courseName;
+    boolean status;
+
+
+    public void inputData(Scanner scanner) {
+
+    }
+}
---
+++ JAVA EXCEPTION_PRACTICE/src/validate/Validator.java
@@ -0,0 +1,9 @@
+package validate;
+
+import java.util.Scanner;
+
+public class Validator {
+    public static void validateId(Scanner scanner,String message, String type){
+
+    }
+}
